dad07fd62225f5be03b6b35ce66abcd29b646b72
sarapejsak <kasjeparas@gmail.com>
2022-09-21 11:39:31 +0200

    jdsfhdfkvjdsejvfkd

---
+++ index2109.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+    <head>
+        <title>PAI</title>
+        <meta charset="utf-8">
+    </head>
+    <body bgcolor="deaded">
+		<?php 
+			/*
+			$punkty = 78;
+			
+			if($punkty >=0 && $punkty <=39){
+				
+				echo "1";
+				
+			}else if($punkty >=40 && $punkty <=54){
+					echo "2";
+					
+				}else if($punkty >=55 && $punkty <=69){
+					echo "3";
+					
+				}else if($punkty >=70 && $punkty <=84){
+					echo "4";
+					
+				}else if($punkty >=85 && $punkty <=94){
+					echo "5";
+					
+				}else if($punkty >=95 && $punkty <=100){
+					echo "6";
+					
+				}
+				*/
+				/*
+				    $j = 10;
+					while($j > -10)
+					{
+						$j--;
+						if($j == 0) continue;
+						echo (10/$j) ."<br>";
+					}
+					*/
+					
+					
+					for($i = -3; $i <= 3; $i++) {
+						
+						if($i == 0) continue;
+						echo $i ."<br>";
+					}
+			
+			
+			
+			
+		
+				?>
+    </body>
+</html>
