d7bc1d895ae5d4aaa12807c5137b0344b26ea49e
sarapejsak <90392165+sarapejsak@users.noreply.github.com>
2022-10-21 14:04:51 +0200

    Add files via upload

--- index2110 v2.php
+++ index2110 v2.php
@@ -4,11 +4,24 @@ session_start();
 <!DOCTYPE html>
 <html>
 <body>
+<form action="./index2110 v2.php" method="post">
 
+		<button type="submit" name="tpop">usuń</button>
+		
+		</form>
 <?php
 // Echo session variables that were set on previous page
 echo $_SESSION["no"] . ".<br>";
 echo $_SESSION["wo"];
+
+
+// destroy the session
+
+
+if(isset($_POST['tpop'])){
+		session_destroy();
+		}
+		
 ?>
 
 </body>
